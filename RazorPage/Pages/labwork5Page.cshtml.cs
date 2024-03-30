4f8ebc995a08371f6595a2eedc8213d089f0ddb6
OnurGuveen <onurguvenvtn@gmail.com>
2024-03-30 12:38:24 +0300

    RazorPages Reloaded

---
+++ RazorPage/Pages/labwork5Page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class labwork5PageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
