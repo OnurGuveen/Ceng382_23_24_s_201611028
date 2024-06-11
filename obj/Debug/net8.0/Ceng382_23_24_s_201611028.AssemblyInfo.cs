ff384d377f3eb1d85b404624d458ce2d6eb713a1
Yazgı Kavukçu <65962091+KodMachine@users.noreply.github.com>
2024-06-12 01:16:07 +0300

    not

--- obj/Debug/net8.0/Ceng382_23_24_s_201611028.AssemblyInfo.cs
+++ obj/Debug/net8.0/Ceng382_23_24_s_201611028.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ceng382_23_24_s_201611028")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e133fefcccb7033cf03f611bf88ff98b132b0d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d5e7f0956a48617e33039919d427296458ea832")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ceng382_23_24_s_201611028")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ceng382_23_24_s_201611028")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
