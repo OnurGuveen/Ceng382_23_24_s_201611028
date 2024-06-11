4e59d3e1116889bc26f19ee7289f629cd0c5be2d
Yazgı Kavukçu <65962091+KodMachine@users.noreply.github.com>
2024-06-12 01:02:56 +0300

    not

--- RazorPage/obj/Debug/net8.0/RazorPage.AssemblyInfo.cs
+++ RazorPage/obj/Debug/net8.0/RazorPage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51fee1cf2be899ecf3e71d02c5cc45aac68b8ecf")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
